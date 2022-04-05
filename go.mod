209c1c8a35e720951619560530901014588fb640
sakashita-motoki <sakashita-motoki@dmm.com>
2022-04-05 12:12:49 +0900

    structにしてみる

--- go.mod
+++ go.mod
@@ -3,5 +3,3 @@ module github.com/sakash1ta90/my_type_pg
 go 1.18
 
 require github.com/go-ozzo/ozzo-validation/v4 v4.3.0
-
-require github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496 // indirect
--- main.go
+++ main.go
@@ -7,6 +7,34 @@ import (
 	"github.com/go-ozzo/ozzo-validation/v4"
 )
 
+type JsonStruct struct {
+	JsonFields   map[string]any
+	ValidateRule map[string][]validation.Rule
+}
+
+func (js *JsonStruct) New(b []byte, vr map[string][]validation.Rule) error {
+	input, err := JsonParse(b)
+	if err != nil {
+		return err
+	}
+	js.JsonFields = input
+	js.ValidateRule = vr
+	return nil
+}
+
+func (js *JsonStruct) Validate() (errors []error) {
+	for k, v := range js.JsonFields {
+		errors = append(errors, validation.Validate(v, js.ValidateRule[k]...))
+	}
+	return
+}
+
+var validateMap = map[string][]validation.Rule{
+	"hoge": {validation.Required, validation.Length(5, 100)},
+	"fuga": {validation.Required, validation.Length(5, 100)},
+	"piyo": {validation.Required, validation.Length(5, 100)},
+}
+
 func JsonParse(b []byte) (map[string]any, error) {
 	var inputValue any
 	if err := json.Unmarshal(b, &inputValue); err != nil {
@@ -16,20 +44,17 @@ func JsonParse(b []byte) (map[string]any, error) {
 }
 
 func main() {
-	input, err := JsonParse([]byte(`{"hoge":null, "fuga":"0", "piyo":3, "foo": [0,4], "bar": "2a"}`))
+	js := JsonStruct{}
+	err := js.New([]byte(`{"hoge":null, "fuga":"0", "piyo":3, "foo": [0,4], "bar": "2a"}`), validateMap)
 	if err != nil {
+		panic(err)
+	}
+
+	if err := js.Validate(); len(err) != 0 {
 		fmt.Println(err)
 	}
 
-	if err := validation.Validate(input["hoge"],
-		validation.Required,
-		validation.Length(5, 100),
-	); err != nil {
-		fmt.Println(err)
-	}
-	// TODO: 他のフィールドのバリデーション
-
-	for key, val := range input {
+	for key, val := range js.JsonFields {
 		fmt.Printf("%s: %+v\n", key, val)
 	}
 }
